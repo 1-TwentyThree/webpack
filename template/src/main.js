a8c21d611261adb274d3af2310f86cda96b2a268
liangyunfei <liangyunfei@yfcloud.com>
2017-03-14 19:36:13 +0800

    增加axios的引用及拦截器

--- template/src/main.js
+++ template/src/main.js
@@ -4,7 +4,8 @@
 {{/if_eq}}
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import iView from 'iview';
+import iView from 'iview'
+import axios from 'axios'
 {{#router}}
 import router from './router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/router}}
@@ -13,6 +14,19 @@ import store from './store'
 Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 Vue.use(iView);
+
+//axios response 可拦截登录和错误
+axios.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+//axios
+Vue.prototype.$http = axios;
 
 /* eslint-disable no-new */
 new Vue({
