04e8e8243c8efe72256363e9376ff0ab89eb09a8
liangyunfei <liangyunfei@yfcloud.com>
2017-03-14 19:12:53 +0800

    修复引入样式的bug

--- template/src/App.vue
+++ template/src/App.vue
@@ -23,7 +23,7 @@ export default {
 </script>
 
 <style lang="less">
-@import "../node_modules/iview/src/styles/index.less"
+@import "../node_modules/iview/src/styles/index.less";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
